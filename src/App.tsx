17dd489d0356430748946a9fdb434bd451930c29
hspark <qkrgustnq@naver.com>
2023-12-07 21:29:29 +0900

    페이지 구성인 Main과 Main 페이지를 이루는 레이아웃 컴포넌트 Header 생성하여 기본적인 구조 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Main from "./pages/Main";
 
 function App() {
-  return <div className="App">hello provit</div>;
+  return (
+    <div className="App">
+      <Main></Main>
+    </div>
+  );
 }
 
 export default App;
